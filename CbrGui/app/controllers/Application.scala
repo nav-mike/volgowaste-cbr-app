35dcceee9308318b69ffcb4c08b14eb0a0e0061a
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2013-12-05 18:51:28 +0400

    Добавлен метод чтения результирующего файла.

--- CbrGui/app/controllers/Application.scala
+++ CbrGui/app/controllers/Application.scala
@@ -2,11 +2,22 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import scala.io.Source
 
 object Application extends Controller {
 
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
+  }
+
+  /**
+   * Метод чтения результирующего файла.
+   * @return Список результатов CBR.
+   */
+  def readResultFile : List[String] = {
+    val result = Source.fromFile("result.txt").getLines().toList
+
+    result
   }
 
 }
