44e38dc956e9a191902a620df124931981fa86f0
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2013-12-07 20:49:25 +0400

    Добавлен проект читалки owl.

---
+++ OwlReader/src/owlreader/OwlReader.java
@@ -0,0 +1,18 @@
+package owlreader;
+
+/**
+ * Главный класс приложения - чтения owl.
+ * @author M. Navrotskiy
+ * @version 1.0
+ */
+public class OwlReader {
+
+    /**
+     * Главная функция приложения.
+     * @param args Аргументы коммандной строки.
+     */
+    public static void main(String[] args) {
+        // TODO текст приложения
+    }
+    
+}
