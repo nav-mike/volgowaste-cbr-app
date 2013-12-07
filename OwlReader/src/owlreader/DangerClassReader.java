f3467129329cd50d4840fea5499c2b498e851474
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2013-12-07 22:33:55 +0400

    Добавлен метод чтения классов опасности из онтологии.

--- OwlReader/src/owlreader/DangerClassReader.java
+++ OwlReader/src/owlreader/DangerClassReader.java
@@ -2,7 +2,15 @@ package owlreader;
 
 import com.hp.hpl.jena.ontology.OntModel;
 import com.hp.hpl.jena.ontology.OntModelSpec;
+import com.hp.hpl.jena.query.Query;
+import com.hp.hpl.jena.query.QueryExecution;
+import com.hp.hpl.jena.query.QueryExecutionFactory;
+import com.hp.hpl.jena.query.QueryFactory;
+import com.hp.hpl.jena.query.QuerySolution;
+import com.hp.hpl.jena.query.ResultSet;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.rdf.model.RDFNode;
+import java.util.ArrayList;
 
 /**
  * Класс чтения классов опасности отходов.
@@ -32,6 +40,28 @@ public class DangerClassReader {
     public DangerClassReader() {
         this.owlModel = ModelFactory.createOntologyModel(OntModelSpec.OWL_DL_MEM);
         this.owlModel.read(this.filename, "RDF/XML-ABBREV");
+    }
+    
+    /**
+     * Метод чтения классов опасности из онтологии.
+     * @return Список классов опасности.
+     */
+    public ArrayList<String> execQuery() {
+        ArrayList result = new ArrayList();
+        
+        Query query = QueryFactory.create(this.request);
+        QueryExecution qexec = QueryExecutionFactory.create(query, this.owlModel);
+        ResultSet results = qexec.execSelect();
+        
+        while (results.hasNext()) {
+            QuerySolution solution = results.nextSolution();
+            RDFNode x = solution.get("object");
+            String name = x.toString();
+            
+            result.add(name);
+        }
+        
+        return result;
     }
     
 }
