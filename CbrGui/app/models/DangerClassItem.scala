4f6b1971e3ab1dbd979de003feb20cbb7f7c3a72
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2013-12-08 17:51:05 +0400

    В класс описания класса опасности добавлены методы доступа к полям класса.

--- CbrGui/app/models/DangerClassItem.scala
+++ CbrGui/app/models/DangerClassItem.scala
@@ -13,4 +13,36 @@ class DangerClassItem(iid : String, ivalue : String) {
   /** Значение класс опасности в онтологии. */
   private var value : String = ivalue
 
+  /**
+   * Метод получения значения идентификатора класса опасности.
+   * @return Значение идентификатора класса опасности.
+   */
+  def getId : String = {
+    this.id
+  }
+
+  /**
+   * Метод изменения значения идентификатора класса опасности.
+   * @param id Значение идентификатора класса опасности.
+   */
+  def setId (id : String) = {
+    this.id = id
+  }
+
+  /**
+   * Метод получения значения класса опасности.
+   * @return Значение класса опасности.
+   */
+  def getValue : String = {
+    this.value
+  }
+
+  /**
+   * Метод изменения значения класса опасности.
+   * @param value Значение класса опасности.
+   */
+  def setValue (value : String) = {
+    this.value = value
+  }
+
 }
