6bd0b3519a9cd2dfbe54d5b348cbc6686ea0e3b2
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2013-12-08 16:48:00 +0400

    Добавлен класс списка классов опасности.

---
+++ OwlReader/src/owlreader/DangerClassResultArray.java
@@ -0,0 +1,16 @@
+package owlreader;
+
+import java.util.ArrayList;
+
+/**
+ * Класс списка классов опасности из онтологии.
+ * @author M. Navrotskiy
+ * @version 1.0
+ */
+public class DangerClassResultArray {
+    
+    /* Поля класса. */
+    /** Список классов опасности из онтологии. */
+    private ArrayList<DangerClassResult> array;
+    
+}
