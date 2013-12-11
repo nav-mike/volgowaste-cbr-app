be268282b844250cba9232272e02cd2477199446
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2013-12-11 12:19:20 +0400

    В класс чтения типов отходов добавлен метод исполнения запроса.

--- OwlReader/src/owlreader/TrashTypeReader.java
+++ OwlReader/src/owlreader/TrashTypeReader.java
@@ -2,7 +2,15 @@ package owlreader;
 
 import com.hp.hpl.jena.ontology.OntModel;
 import com.hp.hpl.jena.ontology.OntModelSpec;
+import com.hp.hpl.jena.query.Query;
+import com.hp.hpl.jena.query.QueryExecution;
+import com.hp.hpl.jena.query.QueryExecutionFactory;
+import com.hp.hpl.jena.query.QueryFactory;
+import com.hp.hpl.jena.query.QuerySolution;
+import com.hp.hpl.jena.query.ResultSet;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.rdf.model.RDFNode;
+import java.util.ArrayList;
 
 /**
  * Класс чтения типов отходов из owl.
@@ -32,6 +40,29 @@ public class TrashTypeReader {
     public TrashTypeReader() {
         this.owlModel = ModelFactory.createOntologyModel(OntModelSpec.OWL_DL_MEM);
         this.owlModel.read(this.filename, "RDF/XML-ABBREV");
+    }
+    
+    /**
+     * Метод чтения типов отходов из онтологии.
+     * @return Список типов отходов из онтологии.
+     */
+    public ArrayList execQuery() {
+        
+        ArrayList result = new ArrayList();
+        
+        Query query = QueryFactory.create(this.request);
+        QueryExecution qexec = QueryExecutionFactory.create(query, this.owlModel);
+        ResultSet results = qexec.execSelect();
+        
+        while (results.hasNext()) {
+            QuerySolution solution = results.nextSolution();
+            RDFNode x = solution.get("entity");
+            String name = x.toString();
+            
+            result.add(name);
+        }
+        
+        return result;
     }
     
 }
