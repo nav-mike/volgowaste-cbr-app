e0d16b1239f645d67db20e1093ea36e245109997
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2013-12-11 12:21:27 +0400

    В читалку owl добавлен класс результата запроса получения типов отходов.

---
+++ OwlReader/src/owlreader/TrashTypeResult.java
@@ -0,0 +1,10 @@
+package owlreader;
+
+/**
+ * Класс результата запроса получения типов отходов.
+ * @author M. Navrotskiy.
+ * @version 1.0.
+ */
+public class TrashTypeResult extends DangerClassResult{
+    
+}
