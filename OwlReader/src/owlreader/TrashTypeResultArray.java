749080763cf12f7605587b68aebaa71354eba3b0
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2013-12-11 12:29:35 +0400

    В класс списка типов отходов добавлен конструктор с параметром.

--- OwlReader/src/owlreader/TrashTypeResultArray.java
+++ OwlReader/src/owlreader/TrashTypeResultArray.java
@@ -1,10 +1,20 @@
 package owlreader;
 
+import java.util.ArrayList;
+
 /**
  * Класс списка типов отходов из онтологии.
  * @author M. Navrotskiy
  * @version 1.0
  */
 public class TrashTypeResultArray extends DangerClassResultArray {
+
+    /**
+     * Конструктор с параметром.
+     * @param list Список типов отходов в онтологии.
+     */
+    TrashTypeResultArray(ArrayList list) {
+        super(list);
+    }
     
 }
