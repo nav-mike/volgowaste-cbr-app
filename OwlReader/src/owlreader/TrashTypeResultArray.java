83b888358743641da1ff5b671b443bbacd65a88f
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2013-12-11 12:23:24 +0400

    В читалку owl добавлен класс списка типов отходов из онтологии.

---
+++ OwlReader/src/owlreader/TrashTypeResultArray.java
@@ -0,0 +1,10 @@
+package owlreader;
+
+/**
+ * Класс списка типов отходов из онтологии.
+ * @author M. Navrotskiy
+ * @version 1.0
+ */
+public class TrashTypeResultArray extends DangerClassResultArray {
+    
+}
