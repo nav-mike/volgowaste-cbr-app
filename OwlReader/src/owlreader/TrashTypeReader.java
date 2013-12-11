a60f774bd27e4de0ac881832e4a259dd52f1e1ae
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2013-12-11 12:06:56 +0400

    В класс чтения типов отходов добавлены поля класса.

--- OwlReader/src/owlreader/TrashTypeReader.java
+++ OwlReader/src/owlreader/TrashTypeReader.java
@@ -1,10 +1,27 @@
 package owlreader;
 
+import com.hp.hpl.jena.ontology.OntModel;
+
 /**
  * Класс чтения типов отходов из owl.
  * @author M. Navrotskiy.
- * @version 1.0
+ * @version 1.0.
  */
 public class TrashTypeReader {
+    
+    /* Поля класса. */
+    
+    /** SPARQL запрос на получение списка отходов из owl. */
+    private final String request = "SELECT ?entity\n" +
+                                            "WHERE {\n" +
+                                            "  ?entity a ?type.\n" +
+                                            "  ?type rdfs:subClassOf* <http://VolgoWaste.owl#Отход>.\n" +
+                                            "}";
+    
+    /** Модель онтологии. */
+    private OntModel owlModel = null;
+    
+    /** Имя файла с онтологией. */
+    private final String filename = "VolgoWaste.owl";
     
 }
