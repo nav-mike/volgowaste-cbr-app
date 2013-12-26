963b95d53154710885f45899d3df935c67a4408b
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2013-12-26 12:13:15 +0400

    Добавлен класс правил адаптаций.

---
+++ CbrApp/src/cbrapp/AdaptationRules.java
@@ -0,0 +1,21 @@
+package cbrapp;
+
+/**
+ * Класс правил адаптации.
+ * @author M. Navrotskiy.
+ * @version 1.0
+ */
+public class AdaptationRules {
+    
+    /**
+     * Метод применения правил адаптации.
+     * @param values Параметры прецедента.
+     * @return Параметры прецедента после адаптации.
+     */
+    public static String[] useRules (String[] values) {
+        String[] result = new String[5];
+        
+        return result;
+    }
+    
+}
