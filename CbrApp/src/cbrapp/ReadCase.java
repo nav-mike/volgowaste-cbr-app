8c419ce8a6af0ac090a3ba8b3f180e5af7be3eac
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2013-12-27 09:26:58 +0400

    Добавлен интерфейс метода получения значений параметров прецедента.

--- CbrApp/src/cbrapp/ReadCase.java
+++ CbrApp/src/cbrapp/ReadCase.java
@@ -1,10 +1,26 @@
 package cbrapp;
 
+import es.ucm.fdi.gaia.ontobridge.OntoBridge;
+import jcolibri.util.OntoBridgeSingleton;
+
 /**
  * Класс чтения прецедента из базы.
  * @author M. Navrotskiy
  * @version 1.0
  */
 public class ReadCase {
+    
+    /**
+     * Метод чтения параметров прецедента из онтологии.
+     * @param uri URI прецедента в онтологии.
+     * @return Массив с параметрами прецедента.
+     */
+    public static String[] getParams (String uri) {
+        
+        String[] result = new String[5];
+        OntoBridge bridge = OntoBridgeSingleton.getOntoBridge();
+        
+        return result;
+    }
     
 }
